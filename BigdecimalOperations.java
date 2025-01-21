f28738696683a598dd5dd45b8d1722ec485695b8
SanthiyaPeriyasamy245 <113085174+SanthiyaPeriyasamy245@users.noreply.github.com>
2025-01-21 18:52:50 +0530

    Add files via upload

---
+++ BigdecimalOperations.java
@@ -0,0 +1,28 @@
+
+
+Big Decimal
+
+this is a immutable class
+
+BigDecimal number1=new BigDecimal("23456245");
+BigDecimal number2=new BigDecimal(23464");
+
+number1.add(number2) // sum the two values.
+System.out.println(number1)
+o/p:23456245
+
+Below Operations can be done using BigDecimal class.
+
+abs(                     add(                     byteValue()              byteValueExact()         compareTo(               divide(
+divideAndRemainder(      divideToIntegralValue(   doubleValue()            equals(                  floatValue()             getClass()
+hashCode()               intValue()               intValueExact()          longValue()              longValueExact()         max(
+min(                     movePointLeft(           movePointRight(          multiply(                negate(                  notify()
+notifyAll()              plus(                    pow(                     precision()              remainder(               toEngineeringString()
+round(			 scaleByPowerOfTen(       shortValue()             shortValueExact()        signum()		     unscaledValue() 
+scale()                  setScale(                subtract(                toBigInteger()           toBigIntegerExact() 
+sqrt(                    stripTrailingZeros()      ulp()                             
+toPlainString()          toString()                wait(   
+
+
+Boolean data type : True/False.
+                
