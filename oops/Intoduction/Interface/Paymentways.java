4e3a676f1964b021fb3fe4b62e427699b33ce9c1
SanthiyaPeriyasamy245 <113085174+SanthiyaPeriyasamy245@users.noreply.github.com>
2025-04-29 18:10:45 +0530

    Update Paymentways.java

--- oops/Intoduction/Interface/Paymentways.java
+++ oops/Intoduction/Interface/Paymentways.java
@@ -2,4 +2,9 @@ public interface Paymentways {
     abstract void gpay();
     abstract void paytm();
     abstract void phonepe();
+    /*
+    In Java, interface members (methods and variables) have default modifiers.
+    By default, interface methods are abstract 
+    and public, and interface variables are public, static, and final. 
+                                */
 }
