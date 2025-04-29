0e0e3562d3f877be2866b08203bed307d5c60f0c
SanthiyaPeriyasamy245 <113085174+SanthiyaPeriyasamy245@users.noreply.github.com>
2025-04-29 18:56:47 +0530

    Update Autoclosable.java

--- ExceptionHandling/Autoclosable.java
+++ ExceptionHandling/Autoclosable.java
@@ -4,7 +4,10 @@ public class Autoclosable {
 
 	public static void main(String[]args)
 	{
-		
+	/* The main difference between AutoCloseable and try-catch-finally in Java lies in resource management.
+		AutoCloseable is an interface that, when implemented, allows resources to be automatically closed at the end of a try-with-resources block,
+		while try-catch-finally is the traditional exception handling mechanism where a finally block is used to ensure resources are closed manuall
+		*/
 		// java 7 feature
 		
 		try(Scanner sc=new Scanner(System.in) ){ // interface cloasable extends autoclosable and it has close() method.
